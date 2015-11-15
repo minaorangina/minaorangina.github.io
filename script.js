4c88127775dad62caf6de4d52987d0eb7c6933c6
Mina <wilhelmina.jane@gmail.com>
2015-11-15 10:30:03 +0000

    adds analytics, folder structure

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 window.onload = function () {
-  var bg = document.getElementsByClassName ("background-div")[0];
+  var bg = document.getElementsByClassName("background-div")[0];
   bg.classList.add("lazyLoad");
   bg.classList.remove("hidden");
+
+  var links = document.getElementsByClassName("links");
+  bg.classList.add("lazyLoad");
+  bg.classList.remove("hidden");
+
+  // links.forEach(function (icon) {
+  //
+  //     icon.addEventListener("mouseover", function() {
+  //         console.log(this);
+  //       //   event.target.classList.add("hovering");
+  //     });
+  // });
+  // links.forEach(function (icon) {
+  //
+  //     icon.addEventListener("mouseout", function() {
+  //
+  //       //   event.target.classList.remove("hovering");
+  //     });
+  // })
+
+
+
+
 }
