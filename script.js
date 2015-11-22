ca442201aeef7504e9eb6f41978ff8f4a164f82b
Mina <wilhelmina.jane@gmail.com>
2015-11-22 09:29:13 +0000

    housekeeping

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
 window.onload = function () {
+
   var bg = document.getElementsByClassName("background-div")[0];
   bg.classList.add("lazyLoad");
   bg.classList.remove("hidden");
 
-  var links = document.getElementsByClassName("links");
-  bg.classList.add("lazyLoad");
-  bg.classList.remove("hidden");
+  // var links = document.getElementsByClassName("links");
+  // bg.classList.add("lazyLoad");
+  // bg.classList.remove("hidden");
 
-  // links.forEach(function (icon) {
-  //
-  //     icon.addEventListener("mouseover", function() {
-  //         console.log(this);
-  //       //   event.target.classList.add("hovering");
-  //     });
-  // });
-  // links.forEach(function (icon) {
-  //
-  //     icon.addEventListener("mouseout", function() {
-  //
-  //       //   event.target.classList.remove("hovering");
-  //     });
-  // })
-
-
-
-
+  var headline = document.getElementsByClassName("headline")[0];
+  headline.classList.add("lazyLoad2");
+  headline.classList.remove("hidden");
 }
